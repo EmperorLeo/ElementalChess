13ee0b7a1d824158f8684583b9039cdfe8d057c4
Leo Feldman <laf22@duke.edu>
2019-05-09 19:34:11 -0400

    king piece done

--- Elemental Chess/Assets/Scripts/KingPiece.cs
+++ Elemental Chess/Assets/Scripts/KingPiece.cs
@@ -19,6 +19,25 @@ public class KingPiece : BasePiece
     public override IEnumerable<ChessSquare> GetAvailableMoves(BasePiece[][] pieces)
     {
         var moves = new List<ChessSquare>();
+        var row = currentSquare.Row - 1;
+        var column = currentSquare.Column - 65;
+
+        for (var r = -1; r < 2; r++)
+        {
+            for (var c = -1; c < 2; c++)
+            {
+                if (c != 0 || r != 0)
+                {
+                    if (row + r < 0 || row + r > 7 || column + c < 0 || column + c > 7 || (pieces[row + r][column + c] != null && pieces[row + r][column + c].Team == Team))
+                    {
+                        continue;
+                    }
+
+                    moves.Add(new ChessSquare(row + r + 1, (char)(column + c + 65)));
+                }
+            }
+        }
+
         return moves;
     }
 }
