47c648dfc04166ae9b8bdb76f404bf701228fcae
Leo Feldman <laf22@duke.edu>
2019-05-09 22:43:32 -0400

    fixed the fucking rotation of the map finally

--- Elemental Chess/Assets/Scripts/CameraRotator.cs
+++ Elemental Chess/Assets/Scripts/CameraRotator.cs
@@ -18,11 +18,15 @@ public class CameraRotator : MonoBehaviour
 
     public void Rotate()
     { 
-        transform.Rotate(0, speed * Time.deltaTime, 0);
+        //transform.Rotate(0, speed * Time.deltaTime, 0);
+        //transform.Rotate(Vector3.up, speed * Time.deltaTime);
+        transform.RotateAround(new Vector3(0.5f, 0, 0.5f), Vector3.up, speed * Time.deltaTime);
     }
 
     public void HandleOffset(float deltaTimeDiff)
     {
-        transform.Rotate(0, speed * deltaTimeDiff, 0);
+        //transform.Rotate(0, speed * deltaTimeDiff, 0);
+        //transform.Rotate(Vector3.up, speed * deltaTimeDiff);
+        transform.RotateAround(new Vector3(0.5f, 0, 0.5f), Vector3.up, speed * deltaTimeDiff);
     }
 }
